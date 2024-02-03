c1988104b185b305e5f09965c65d8f9f40221067
Sachinthana DA <136717556+AshenSachinthana@users.noreply.github.com>
2024-02-03 17:36:57 +0530

    Learned and practiced using list in Python

--- Python/list_comprehensions.py
+++ Python/list_comprehensions.py
@@ -1,1 +1,14 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+x_list = list(range(0, x+1))
+y_list = list(range(0, y+1))
+z_list = list(range(0, z+1))
+
+result = [[x, y, z] for x in x_list for y in y_list for z in z_list]
+
+selected_lists = [inner_list for inner_list in result if sum(inner_list) != n]
+print(selected_lists)
 
