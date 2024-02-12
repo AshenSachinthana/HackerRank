e4378be1db19246e79c7d6d2e2099cc1002d57b1
Sachinthana DA <136717556+AshenSachinthana@users.noreply.github.com>
2024-02-12 11:33:07 +0530

    Implemented sorting algorithm for HackerRank problem
    you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.

--- Python/sort.py
+++ Python/sort.py
@@ -1,1 +1,18 @@
+# number of scores
+n = int(input())
+
+# scores convert them to integers
+scores = list(map(int, input().split()))
+
+# Remove duplicates
+unique_scores = list(set(scores))
+
+# Bubble sort 
+for i in range(len(unique_scores)):
+    for j in range(len(unique_scores) - 1):
+        if unique_scores[j] < unique_scores[j + 1]:
+            unique_scores[j], unique_scores[j + 1] = unique_scores[j + 1], unique_scores[j]
+
+# second element from the sorted list
+print(unique_scores[1])
 
