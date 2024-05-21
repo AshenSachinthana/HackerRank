cdb3b955a0a3d20c60c087fe8490ac1351e21910
Sachinthana DA <136717556+AshenSachinthana@users.noreply.github.com>
2024-05-21 16:24:45 +0530

    Create stdin_stdout.java
    One popular way to read input from stdin is by using the Scanner class and specifying the Input Stream as System.in

---
+++ Java/stdin_stdout.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        // Close the scan
+        scan.close();
+    }
+}
